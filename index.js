96431086d9b5bc0045d4c7a0a4b080e763c828e5
Anjanique M <99520247+Milkdromeda-333@users.noreply.github.com>
2022-05-22 21:43:32 -0700

    Add files via upload
    I will be attempting to add local storage functionality.

---
+++ index.js
@@ -0,0 +1,34 @@
+// my global variables
+let form = document.form
+let input = document.form.input;
+let submitBtn = document.form.submitBtn;
+let fontResetBtn = document.getElementById("fontResetBtn");
+let list = document.querySelector("ul");
+let listItem = document.querySelector("li");
+let myPlaceHolder = document.querySelector("#placeholder");
+
+// my global event listeners
+
+/*let myPlaceHolder = document.createElement("p");
+myPlaceHolder.textContent = "You have nothing in your list at the moment";
+list.append(myPlaceHolder);
+if (list.children.length = 0) {
+  myPlaceHolder.remove();
+}*/
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  myPlaceHolder.remove();
+  let inputValue = document.form.input.value;
+  let newListItem = document.createElement("li");
+  newListItem.textContent = inputValue;
+  list.append(newListItem);
+  form.reset();
+  newListItem.addEventListener("click", (e) => e.target.remove());
+});
+
+fontResetBtn.addEventListener("click", function () {
+  document.body.style.fontFamily = "var(--regular)";
+  document.body.style.fontSize = "30px";
+  input.style.fontFamily = "var(--regular)";
+  input.style.fontSize = "25px";
+});
